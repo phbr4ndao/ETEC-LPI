5ec1e5864515cf5c00782ac5f9b833b3d58340b3
Pedro Brandão <phbr4ndao@gmail.com>
2025-10-31 17:22:38 -0300

    Delete Facilitador-De-Compras-main directory

--- Facilitador-De-Compras-main/App.Core/Models/MercadoModel.cs
+++
@@ -1,8 +0,0 @@
-namespace App.Core.Model
-{
-	public class MercadoModel
-	{
-		public string Nome { get; set; } = string.Empty;
-		public List<ProdutoModel> ProdutoList { get; set; } = new();
-	}
-}
