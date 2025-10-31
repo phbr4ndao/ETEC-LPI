48bae011e15f17e66e7096081d67384beaec3e9e
Pedro Brandão <phbr4ndao@gmail.com>
2025-10-31 17:23:51 -0300

    Add files via upload

---
+++ Facilitador-De-Compras-main/App.Core/Models/ProdutoModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace App.Core.Model
+{
+    public class ProdutoModel
+    {
+
+        public string Nome { get; set; } = string.Empty;
+        public string Categoria { get; set; } = string.Empty;
+        public string Ean { get; set; } = string.Empty;
+        public string UnidadeDeVendas { get; set; } = string.Empty;
+        public double Preco { get; set; } = 0;
+
+        public string MercadoNome { get; set; } = string.Empty;
+
+        public static ProdutoModel? ConvertCsvTextLineToModel(string? text)
+        {
+            if (text == null) return null;
+
+            string[] parts = text?.Split(",");
+
+            if (parts.Count() < 6)
+            {
+                Console.WriteLine(text + ":" + parts.Count());
+                return null;
+            }
+
+            
+            ProdutoModel model = new();
+
+            model.Nome = parts[0];
+            model.Ean = parts[1];
+            model.Categoria = parts[2];
+            model.UnidadeDeVendas = parts[3];
+            model.MercadoNome = parts[4];
+            model.Preco = Double.Parse(parts[5]);
+
+            return model;
+        }
+    }
+}
